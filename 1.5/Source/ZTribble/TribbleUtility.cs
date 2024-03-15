bb2dfb0b47c3c5d0a0acdbac45bffe0dc4182efd
Zylleon <56230821+Zylleon@users.noreply.github.com>
2024-03-14 19:18:02 -0600

    1.5 update

---
+++ 1.5/Source/ZTribble/TribbleUtility.cs
@@ -0,0 +1,39 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace ZTribble
+{
+    public static class TribbleUtility
+    {
+        public static bool CanSpawnNow(IncidentParms parms)
+        {
+            //IncidentWorker.CanFireNow(parms)
+
+            IncidentDef def = IncidentDef.Named("ZTrib_TribbleArrival");
+            if (def.Worker.CanFireNow(parms))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        public static void ResetInterval(Map map)
+        {
+            FiringIncident fi = new FiringIncident();
+            fi = new FiringIncident(IncidentDef.Named("ZTrib_TribbleArrival"), null, null);
+            map.StoryState.Notify_IncidentFired(fi);
+        }
+
+
+
+
+
+
+    }
+}
